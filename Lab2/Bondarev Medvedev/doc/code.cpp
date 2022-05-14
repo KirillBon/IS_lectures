3ae65dea80340023dfca407742eb3ff58fbffc6d
KirillBon <86649102+KirillBon@users.noreply.github.com>
2022-05-14 10:54:38 +0300

    Add files via upload

--- Lab2/Bondarev Medvedev/doc/code.cpp
+++ Lab2/Bondarev Medvedev/doc/code.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 using namespace std;
 
-enum States
+enum class Event
+{
+	PUSH_ON,
+	PUSH_OFF,
+	GETTINGCOORDINATES,
+	Move,
+	Turn_Right,
+	Turn_Left,
+	Paint_Wall,
+	Paint_Floor,
+	Return_Place
+
+};
+
+enum class States
 {
 	ON,
 	OFF,
-	GETTINGCOORDINATES,
+	GETTINGCOORDINATED,
 	MoveForward,
 	TurnRight,
 	TurnLeft,
@@ -13,114 +27,197 @@ enum States
 	PaintFloor,
 	ReturnPlace
 
-}
-;
+};
 
-class Robot {
+class Robot 
+{
 	States state;
 public:
-	void on() {
+	void pevent(const Event& e)
+	{
+		switch (e)
+		{
+		case Event::Move:
+			move();
+			state = States::MoveForward;
+			break;
+
+		case Event::Turn_Left:
+			turnLeft();
+			state = States::TurnLeft;
+			break;
+		case Event::Turn_Right:
+			turnRight();
+			state = States::TurnRight;
+			break;
+		case Event::Paint_Floor:
+			paint_floor();
+			state = States::PaintFloor;
+			break;
+		case Event::Paint_Wall:
+			paint_wall();
+			state = States::PaintWall;
+			break;
+		case Event::GETTINGCOORDINATES:
+			getting_coordinated();
+			state = States::GETTINGCOORDINATED;
+			break; 
+		case Event::PUSH_OFF:
+			off();
+			state = States::OFF;
+			break;
+		case Event::PUSH_ON:
+			on();
+			state = States::ON;
+			break;
+		case Event::Return_Place:
+			return_on_his_place();
+			state = States::ReturnPlace;
+			break;
+		}
+	}
+
+	void on() 
+	{
 		state = States::ON;
 	}
 
-	void off() {
+	void off() 
+	{
 		state = States::OFF;
 	}
 
-	void getting_coordinated() {
-		state = States::GETTINGCOORDINATES;
+	void getting_coordinated() 
+	{
+		state = States::GETTINGCOORDINATED;
 	}
 
-	void move() {
+	void move() 
+	{
 		state = States::MoveForward;
 	}
 
-	void turnLeft() {
+
+	{
 		state = States::TurnLeft;
 	}
 
-	void turnRight() {
+	void turnRight() 
+	{
 		state = States::TurnRight;
 	}
 
-	void paint_wall() {
+	void paint_wall() 
+	{
 		state = States::PaintWall;
 
 	}
 
-	void paint_floor() {
+	void paint_floor() 
+	{
 		state = States::PaintFloor;
 
 	}
 
-	void return_on_his_place() {
+	void return_on_his_place() 
+	{
 		state = States::ReturnPlace;
 
 	}
 
-}
-;
+};
 
-class CameraSystem {
+class CameraSystem
+{
 	States state;
 public:
-	void detected_graffiti() {
-		state = States::GETTINGCOORDINATES;
+	void detected_graffiti() 
+	{
+		state = States::GETTINGCOORDINATED;
 	}
 
-	void call_user() {
-		state = States::GETTINGCOORDINATES;
+	void call_user() 
+	{
+		state = States::GETTINGCOORDINATED;
 	}
 
-	void call_robot_coordinates() {
-		state = States::GETTINGCOORDINATES;
+	void call_robot_coordinates() 
+	{
+		state = States::GETTINGCOORDINATED;
 	}
 }
 ;
-int main() {
-	Robot r;
-	while (true)
+class StrategyCommand
+{
+public:
+	virtual Event getEvent() = 0;
+};
+
+class InternetCommand : public StrategyCommand
+{
+public:
+	Event getEvent()
+	{
+		return Event::PUSH_OFF;
+	}
+};
+
+class ConsoleCommand : public StrategyCommand
+{
+public:
+	Event getEvent()
 	{
 		char c;
-		cin >> c;
-		switch (c)
-		{
-		case 'm':
-			r.move();
-			break;
+        while (true)
+        {
+	        cin >> c;
+	        switch (c)
+	        {
+	        case 'm':
+		        return Event::Move;
 
-		case 'l':
-			r.turnLeft();
-			break;
+	        case 'l':
+		        return Event::Turn_Left;
 
-		case 'r':
-			r.turnRight();
-			break;
+	        case 'r':
+		        return Event::Turn_Right;
 
-		case 'f':
-			r.paint_floor();
-			break;
+	        case 'f':
+		       return Event::Paint_Floor;
 
-		case 'w':
-			r.paint_wall();
-			break;
+	        case 'w':
+		        return Event::Paint_Wall;
 
-		case 'g':
-			r.getting_coordinated();
-			break;
+	        case 'g':
+		        return Event::GETTINGCOORDINATES;
 
-		case '0':
-			r.off();
-			break;
+	        case '0':
+		        return Event::PUSH_OFF;
 
-		case '1':
-			r.on();
-			break;
+	        case '1':
+		        return Event::PUSH_ON;
 
-		case '0p':
-			r.return_on_his_place();
-			break;
-		}
+	        case 't':
+		        return Event::Return_Place;
+	        default:
+		        cout << "incorrect";
+	        }
+        } 
+     }
+};
 
+
+int main() 
+{
+
+	Robot r;
+	StrategyCommand* cmd;
+	cmd = new ConsoleCommand();
+
+	while (true)
+	{
+		Event e = cmd->getEvent();
+		r.pevent(e);
 	}
 }
+	
